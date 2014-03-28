b9a78a73ab2c726914d5106f355666d1c8e1e04e
Miaz Akemapa <miazakemapa@gmail.com>
2014-03-28 15:56:15 +0700

    Add Translation Support
    - Adding regnomail-en_US.po (and .mo) for base languages.
    - Modify regnomail.php to support translations

--- regnomail.php
+++ regnomail.php
@@ -1,11 +1,12 @@
 <?php
 /*
-Plugin Name: Regnomail
-Plugin URI: http://www.vnetware.com/
-Description: Simple plugin to disable sending email confirmation during user registration and customize registration form, like display password form, first & last name, and change login / registration logo image.
-Version: 1.0
-Author: Miaz Akemapa
-Author URI: http://www.vnetware.com/
+* Plugin Name: Regnomail
+* Plugin URI: http://www.vnetware.com/
+* Description: Simple plugin to disable sending email confirmation during user registration and customize registration form, like display password form, first & last name, and change login / registration logo image.
+* Version: 1.0
+* Author: Miaz Akemapa
+* Author URI: http://www.vnetware.com/
+* Text Domain: regnomail
 */
 
 Class RegNoMail {
@@ -26,6 +27,9 @@ Class RegNoMail {
         add_filter( 'login_headertitle', array($this,'rgm_login_logo_url_title'));
         
         add_action('admin_init', array($this,'rgm_options_init'));
+        
+        //Translation
+        add_action('init', array($this,'rgm_lang_init'));
     }
     
     public function get_rgm_options(){
@@ -37,6 +41,10 @@ Class RegNoMail {
     
     public function add_rgm_menu(){
         add_options_page("Regnomail Settings", "Regnomail Settings", 'manage_options', "regnomail", array($this,"rgm_settings"));
+    }
+    
+    function rgm_lang_init(){
+        load_plugin_textdomain('regnomail', false, dirname(plugin_basename(__FILE__))."/languages/");
     }
     
     function rgm_registration_form() {
@@ -55,23 +63,23 @@ Class RegNoMail {
             add_filter( 'gettext', array($this,'rgm_password_email_text')); 
         ?>
         <p>
-		<label for="password">Password<br/>
+		<label for="password"><?php _e('Password','regnomail'); ?><br/>
 		<input id="password" class="input" type="password" tabindex="10" size="25" value="" name="password" />
 		</label>
 	</p>
 	<p>
-		<label for="repeat_password">Repeat password<br/>
+		<label for="repeat_password"><?php _e('Repeat Password','regnomail'); ?><br/>
 		<input id="repeat_password" class="input" type="password" tabindex="11" size="25" value="" name="repeat_password" />
 		</label>
 	</p>
         <?php }
         if($rgm_fullname != "") { ?>
         <p>
-            <label for="first_name"><?php _e('First Name')?><br />
+            <label for="first_name"><?php _e('First Name', 'regnomail')?><br />
             <input name="first_name" type="text" size="39" tabindex="12" id="first_name" value="<?php echo esc_attr(stripslashes($first_name)); ?>" /></label>
         </p>
         <p>
-            <label for="last_name"><?php _e('Last Name')?><br />
+            <label for="last_name"><?php _e('Last Name', 'regnomail')?><br />
             <input name="last_name" type="text" size="39" tabindex="13" id="last_name" value="<?php echo esc_attr(stripslashes($last_name)); ?>" /></label>
         </p>
         <?php }
@@ -84,16 +92,16 @@ Class RegNoMail {
         
         if($rgm_fullname != "" AND $rgm_fullname == "1"){
             if ( empty( $_POST['first_name'] ) )
-                $errors->add( 'first_name_error', __('<strong>ERROR</strong>: You must include a first name.','vnetware') );
+                $errors->add( 'first_name_error', __('<strong>ERROR</strong>: You must include a first name.','regnomail') );
             if ( empty( $_POST['last_name'] ) )
-                $errors->add( 'last_name_error', __('<strong>ERROR</strong>: You must include a Last name.','vnetware') );
+                $errors->add( 'last_name_error', __('<strong>ERROR</strong>: You must include a Last name.','regnomail') );
         }
         if($rgm_password != "" AND $rgm_password == "1"){
             if ( $_POST['password'] !== $_POST['repeat_password'] ) {
-                    $errors->add( 'passwords_not_matched', __("<strong>ERROR</strong>: Passwords must match.",'vnetware') );
+                    $errors->add( 'passwords_not_matched', __("<strong>ERROR</strong>: Passwords must match.",'regnomail') );
             }
             if ( strlen( $_POST['password'] ) < 8 ) {
-                    $errors->add( 'password_too_short', __("<strong>ERROR</strong>: Passwords must be at least eight characters long",'vnetware') );
+                    $errors->add( 'password_too_short', __("<strong>ERROR</strong>: Passwords must be at least eight characters long",'regnomail') );
             }
         }
 	/*if ( $_POST['are_you_human'] !== get_bloginfo( 'name' ) ) {
@@ -125,7 +133,7 @@ Class RegNoMail {
     
     function rgm_password_email_text($text){
         if ( $text == 'A password will be e-mailed to you.' ) {
-		$text = 'Password must be at least 8 characters long.';
+		$text = __('Password must be at least 8 characters long.','regnomail');
 	}
 	return $text;
     }
@@ -140,7 +148,7 @@ Class RegNoMail {
         }
         
         echo '<div id="icon-options-general" class="icon32"><br /></div>  ';
-        echo "<span><h2 style='padding-top:10px;'>" . __( 'Regnomail Settings') . "</h2></span>  ";
+        echo "<span><h2 style='padding-top:10px;'>" . __('Regnomail Settings','regnomail') . "</h2></span>  ";
         ?>
 <script>
 jQuery(document).ready(function($) {
@@ -188,51 +196,51 @@ jQuery(document).ready(function($) {
                         ?>
                         <table class="form-table">
                             <tbody>
-                                <tr valign="top"><th scope="row">Email Settings</th>
+                                <tr valign="top"><th scope="row"><?php _e('Email Settings','regnomail');?></th>
                                     <td>
                                         <fieldset>
-                                            <legend class="screen-reader-text"><span>Email Settings</span></legend>
+                                            <legend class="screen-reader-text"><span><?php _e('Email Settings','regnomail');?></span></legend>
                                             <label for="disable_email_user">
                                             <input name="rgm_options[disable_email_user]" type="checkbox" id="disable_email_user" value="1" <?php if($options['disable_email_user'] == "1") echo "checked"; ?>>
-                                            Disable the automatic welcome email sent to users</label>
+                                            <?php _e('Disable the automatic welcome email sent to users','regnomail');?></label>
                                             <br />
                                             <label for="disable_email_admin">
                                             <input name="rgm_options[disable_email_admin]" type="checkbox" id="disable_email_admin" value="1" <?php if($options['disable_email_admin'] == "1") echo "checked"; ?>>
-                                            Disable new user notification email to admin</label>
+                                            <?php _e('Disable new user notification email to admin','regnomail');?></label>
                                             <br />
                                             <label for="disable_change_pwd">
                                             <input name="rgm_options[disable_change_pwd]" type="checkbox" id="disable_change_pwd" value="1" <?php if($options['disable_change_pwd'] == "1") echo "checked"; ?>>
-                                            Disable user password change notification email to admin</label>
+                                            <?php _e('Disable user password change notification email to admin','regnomail');?></label>
                                             <br />
                                         </fieldset>
                                     </td>
                                 </tr>
-                                <tr valign="top"><th scope="row">Registration Form</th>
+                                <tr valign="top"><th scope="row"><?php _e('Registration Form','regnomail');?></th>
                                     <td>
                                         <fieldset>
-                                            <legend class="screen-reader-text"><span>Registration Form</span></legend>
+                                            <legend class="screen-reader-text"><span><?php _e('Registration Form','regnomail');?></span></legend>
                                             <label for="show_full_name">
                                             <input name="rgm_options[show_full_name]" type="checkbox" id="show_full_name" value="1" <?php if($options['show_full_name'] == "1") echo "checked"; ?>>
-                                            Show First & Last Name form</label>
+                                            <?php _e('Show First & Last Name form','regnomail');?></label>
                                             <br />
                                             <label for="show_password">
                                             <input name="rgm_options[show_password]" type="checkbox" id="show_password" value="1" <?php if($options['show_password'] == "1") echo "checked"; ?>>
-                                            Show Password form</label>
+                                            <?php _e('Show Password form','regnomail');?></label>
                                             <br />
                                         </fieldset>
                                     </td>
                                 </tr>
-                                <tr valign="top"><th scope="row">Custom Login Logo</th>
+                                <tr valign="top"><th scope="row"><?php _e('Custom Login Logo','regnomail');?></th>
                                     <td><input type="text" id="rgm_login_logo" name="rgm_options[custom_login_logo]" value="<?php echo $options['custom_login_logo']; ?>" class="regular-text" />
-                                    <input id="rgm_upload_button" type="button" class="button rgm_upload_button" value="<?php _e( 'Select Image', 'rgm' ); ?>" />
+                                    <input id="rgm_upload_button" type="button" class="button rgm_upload_button" value="<?php _e( 'Select Image', 'regnomail' ); ?>" />
                                     <span><img id="rgm_login_image" src="" class="floatright" /></span>
                                     </td>
                                 </tr>
                             </tbody>
                         </table>
                         <p class="submit">
-                            <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
-                            <input type="button" id="rgm_reset_default" class="button-primary" value="<?php _e('Reset To Default') ?>" />
+                            <input type="submit" class="button-primary" value="<?php _e('Save Changes','regnomail') ?>" />
+                            <input type="button" id="rgm_reset_default" class="button-primary" value="<?php _e('Reset To Default','regnomail') ?>" />
                         </p>
                     </form>
 <form id="form_rgm_reset" name="form_rgm_reset" action="" method="get">
@@ -288,23 +296,23 @@ if(!function_exists('wp_new_user_notification')){
         
 	$blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
 
-	$message  = sprintf(__('New user registration on your site %s:'), $blogname) . "\r\n\r\n";
-	$message .= sprintf(__('Username: %s'), $user->user_login) . "\r\n\r\n";
-	$message .= sprintf(__('E-mail: %s'), $user->user_email) . "\r\n";
+	$message  = sprintf(__('New user registration on your site %s:','regnomail'), $blogname) . "\r\n\r\n";
+	$message .= sprintf(__('Username: %s','regnomail'), $user->user_login) . "\r\n\r\n";
+	$message .= sprintf(__('E-mail: %s','regnomail'), $user->user_email) . "\r\n";
         
         if(!isset($rgm_options['disable_email_admin']) OR $rgm_options['disable_email_admin'] != '1'){
-            @wp_mail(get_option('admin_email'), sprintf(__('[%s] New User Registration'), $blogname), $message);
+            @wp_mail(get_option('admin_email'), sprintf(__('[%s] New User Registration','regnomail'), $blogname), $message);
         }
 
 	if ( empty($plaintext_pass) )
 		return;
 
-	$message  = sprintf(__('Username: %s'), $user->user_login) . "\r\n";
-	$message .= sprintf(__('Password: %s'), $plaintext_pass) . "\r\n";
+	$message  = sprintf(__('Username: %s','regnomail'), $user->user_login) . "\r\n";
+	$message .= sprintf(__('Password: %s','regnomail'), $plaintext_pass) . "\r\n";
 	$message .= wp_login_url() . "\r\n";
         
         if(!isset($rgm_options['disable_email_user']) OR $rgm_options['disable_email_user'] != '1'){
-            wp_mail($user->user_email, sprintf(__('[%s] Your username and password'), $blogname), $message);
+            wp_mail($user->user_email, sprintf(__('[%s] Your username and password','regnomail'), $blogname), $message);
         }
 
     }
